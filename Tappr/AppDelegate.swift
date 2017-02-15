c2893be07e94f6a4df5c6bf092dbf4f01a0ae33b
Brett Hales <brett@mytappr.com>
2017-02-15 10:07:33 +1000

    Initial Commit

---
+++ Tappr/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tappr
+//
+//  Created by Brett Hales on 15/2/17.
+//  Copyright © 2017 Brett Hales. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tappr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tappr
+//
+//  Created by Brett Hales on 15/2/17.
+//  Copyright © 2017 Brett Hales. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
